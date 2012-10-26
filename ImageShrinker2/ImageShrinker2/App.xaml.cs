45ca3e9ac5f76bc85cb5c9ab56d171ae87615389
Peter <peter.friedland@gmx.de>
2012-10-26 11:29:14 +0200

    log4net

--- ImageShrinker2/ImageShrinker2/App.xaml.cs
+++ ImageShrinker2/ImageShrinker2/App.xaml.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿
 using System.Windows;
+using log4net.Config;
 
 namespace ImageShrinker2
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            XmlConfigurator.Configure();
+        }
     }
 }
